4fdd404c222165f10db73e9d92684d815114bd1c
Preeti Yuankrathok <preetisatit@gmail.com>
2022-12-09 17:53:20 +0700

    Add getter function

--- csms/__init__.py
+++ csms/__init__.py
@@ -1,3 +1,3 @@
 # pylint: disable=unused-import
-from .functions import load_env
-from .services import CsmsService
+from .functions import get, get_json, load_env
+from .services import Csms
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,53 +1,74 @@
-import os
 import unittest
 from unittest import mock
 
 import csms
 
 
-@mock.patch('csms.functions.services.CsmsService')
+@mock.patch('csms.functions.services.Csms')
 class LoadEnvTest(unittest.TestCase):
 
-    def test_input_only_secret_name(self, mock_service):
-        csms.functions.load_env('secret')
+    def test_input_only_secret_name(self, mock_csms):
+        csms.load_env('secret')
 
-        mock_service.assert_called_once_with()
-        mock_service.return_value.get_json_secret.assert_called_once_with('secret', 'latest')
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.load_env.assert_called_once_with('secret', 'latest')
 
-    def test_input_secret_name_and_version_id(self, mock_service):
-        csms.functions.load_env('secret', 'v1')
+    def test_input_secret_name_and_version_id(self, mock_csms):
+        csms.load_env('secret', 'v1')
 
-        mock_service.assert_called_once_with()
-        mock_service.return_value.get_json_secret.assert_called_once_with('secret', 'v1')
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.load_env.assert_called_once_with('secret', 'v1')
 
-    def test_secret_name_in_env(self, mock_service):
-        os.environ['HUAWEICLOUD_SECRET_NAME'] = 'secret'
+    def test_input_only_version_id(self, mock_csms):
+        csms.load_env(version_id='v1')
 
-        csms.functions.load_env()
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.load_env.assert_called_once_with(None, 'v1')
 
-        mock_service.assert_called_once_with()
-        mock_service.return_value.get_json_secret.assert_called_once_with('secret', 'latest')
+    def test_no_arg(self, mock_csms):
+        csms.load_env()
 
-    def test_no_arg(self, mock_service):
-        self.assertRaises(AssertionError, csms.functions.load_env)
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.load_env.assert_called_once_with(None, 'latest')
 
-        mock_service.assert_not_called()
-        mock_service.return_value.get_json_secret.assert_not_called()
 
-    def test_setting_single_env(self, mock_service):
-        mock_service.return_value.get_json_secret.return_value = {'KEY': 'VALUE'}
+@mock.patch('csms.functions.services.Csms')
+class GetTest(unittest.TestCase):
 
-        csms.functions.load_env('secret')
+    def test_input_only_secret_name(self, mock_csms):
+        csms.get('secret')
 
-        self.assertEqual(os.environ.get('KEY'), 'VALUE')
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.get_raw_secret.assert_called_once_with('secret', 'latest')
 
-    def test_setting_multiple_env(self, mock_service):
-        mock_service.return_value.get_json_secret.return_value = {
-            'KEY1': 'VALUE1',
-            'KEY2': 'VALUE2',
-        }
+    def test_input_secret_name_and_version_id(self, mock_csms):
+        csms.get('secret', 'v1')
 
-        csms.functions.load_env('secret')
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.get_raw_secret.assert_called_once_with('secret', 'v1')
 
-        self.assertEqual(os.environ.get('KEY1'), 'VALUE1')
-        self.assertEqual(os.environ.get('KEY2'), 'VALUE2')
+    def test_output(self, mock_csms):
+        mock_csms.return_value.get_raw_secret.return_value = 'test'
+
+        self.assertEqual(csms.get('secret'), 'test')
+
+
+@mock.patch('csms.functions.services.Csms')
+class GetJsonTest(unittest.TestCase):
+
+    def test_input_only_secret_name(self, mock_csms):
+        csms.get_json('secret')
+
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.get_json_secret.assert_called_once_with('secret', 'latest')
+
+    def test_input_secret_name_and_version_id(self, mock_csms):
+        csms.get_json('secret', 'v1')
+
+        mock_csms.assert_called_once_with()
+        mock_csms.return_value.get_json_secret.assert_called_once_with('secret', 'v1')
+
+    def test_output(self, mock_csms):
+        mock_csms.return_value.get_json_secret.return_value = {'key': 'value'}
+
+        self.assertEqual(csms.get_json('secret'), {'key': 'value'})
